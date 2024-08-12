ec0360bae118a1de8fc3bac8009b72abd79e00cd
JihuiTanUchicago <tanjihui@uchicago.edu>
2024-08-12 19:42:26 -0400

    stringstream

---
+++ cpp/StringStream.cpp
@@ -0,0 +1,29 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+	// Complete this function
+    stringstream ss(str);
+    vector<int> vec;
+    int num;
+    char ch;
+    while(ss >> num){
+        vec.push_back(num);
+        ss >> ch;
+    }
+    
+    return vec;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
